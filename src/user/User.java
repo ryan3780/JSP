912c10d842c221345b005658dc209802c750a7db
since890513 <since890513@naver.com>
2019-08-28 10:54:57 +0900

    Update User.java

--- src/user/User.java
+++ src/user/User.java
@@ -1,9 +1,11 @@
 //JSP 자바빈즈라고 한다. 이 자바 클래스를!!
 package user;
 
+// Model로 사용되는 건 lombok 한 번 찾아서 써보는걸 추천드려요
+// get,set 메소드 만들 필요 없음
 public class User {
 
-	private String userID;
+	private String userID; // 왠만하면 카멜기법이 되도록 userId 가 되는게 좋습니다.
 	private String userPassword;
 	private String userName;
 	private String userGender;
